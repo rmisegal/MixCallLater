cadf0a1101b757725a119d3ae866391dabdad9d1
Haris Botic <haris.botic96@gmail.com>
2017-09-21 19:36:14 +0200

    Trying to fix receivers

--- app/src/main/java/com/example/edry/finalcalllater/IncomingPhoneCallReceiver.java
+++ app/src/main/java/com/example/edry/finalcalllater/IncomingPhoneCallReceiver.java
@@ -3,7 +3,10 @@ package com.example.edry.finalcalllater;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
 
 public class IncomingPhoneCallReceiver extends BroadcastReceiver {
 
@@ -16,54 +19,13 @@ public class IncomingPhoneCallReceiver extends BroadcastReceiver {
 
         //System.out.println("Flow: IncomingPhoneCallReceiver : onReceive  ");
 
-        PhoneState = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
-
-        adiitionalPhoneNumber = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-
-        int curState = getState();
-
-        switch (curState) {
-
-            case TelephonyManager.CALL_STATE_IDLE:
-
-                MyPhoneState SoundUp = new MyPhoneState();
-
-                SoundUp.onCallStateChanged(context,0,null);
-
-                break;
-
-            case TelephonyManager.CALL_STATE_RINGING:
-
-                Intent newCallInIntent = new Intent(context, OnPhoneCallService.class);
-
-                newCallInIntent.putExtra("EXTRA_NUMBER",adiitionalPhoneNumber);
-
-                newCallInIntent.putExtra("CASE","INCOMING");
-
-                context.startService(newCallInIntent);
-
-                break;
-
-            case TelephonyManager.CALL_STATE_OFFHOOK:
-
-
-                break;
-
-            default:
-
-
-                break;
-
-
-        }
+        TelephonyManager tmgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        MyPhoneStateListener PhoneListener = new MyPhoneStateListener(context, intent);
+        tmgr.listen(PhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
 
         //System.out.println("Flow: IncomingPhoneCallReceiver : case " + PhoneState +" " + adiitionalPhoneNumber);
 
-
     }
-
-
-
 
     private int getState( )
     {
@@ -80,4 +42,60 @@ public class IncomingPhoneCallReceiver extends BroadcastReceiver {
         }
         return state;
     };
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+
+        Context context;
+        Intent intent;
+
+        MyPhoneStateListener(Context context, Intent intent) {
+            this.context = context;
+            this.intent = intent;
+        }
+
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            Log.d("MyPhoneListener",state+"   incoming no:"+incomingNumber);
+
+            PhoneState = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+
+            adiitionalPhoneNumber = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            int curState = getState();
+
+            switch (curState) {
+
+                case TelephonyManager.CALL_STATE_IDLE:
+
+                    MyPhoneState SoundUp = new MyPhoneState();
+
+                    SoundUp.onCallStateChanged(context,0,null);
+                    Toast.makeText(context, "CURRENT STATE: IDLE STATE", Toast.LENGTH_LONG).show();
+
+                    break;
+
+                case TelephonyManager.CALL_STATE_RINGING:
+
+                    Intent newCallInIntent = new Intent(context, OnPhoneCallService.class);
+
+                    newCallInIntent.putExtra("EXTRA_NUMBER",adiitionalPhoneNumber);
+
+                    newCallInIntent.putExtra("CASE","INCOMING");
+
+                    context.startService(newCallInIntent);
+
+                    Toast.makeText(context, "CURRENT STATE: RINGING STATE", Toast.LENGTH_LONG).show();
+
+                    break;
+
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Toast.makeText(context, "CURRENT STATE: OFFHOOK STATE", Toast.LENGTH_LONG).show();
+                    break;
+
+                default:
+                    Toast.makeText(context, "CURRENT STATE: DEFAULT STATE", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
 }
